a26a6033b3df43d8dab82f2944d7aeb3d96d345b
Ryan Omasta <git@ryand.ca>
2025-05-21 13:54:56 -0600

    cinny-desktop: 4.7.0 -> 4.7.1
    Release notes: https://github.com/cinnyapp/cinny-desktop/releases/tag/v4.7.1
    Changelog: https://github.com/cinnyapp/cinny-desktop/compare/v4.7.0...v4.7.1

--- pkgs/by-name/ci/cinny-desktop/package.nix
+++ pkgs/by-name/ci/cinny-desktop/package.nix
@@ -15,29 +15,29 @@
   moreutils,
 }:
 
-rustPlatform.buildRustPackage rec {
+rustPlatform.buildRustPackage (finalAttrs: {
   pname = "cinny-desktop";
   # We have to be using the same version as cinny-web or this isn't going to work.
-  version = "4.7.0";
+  version = "4.7.1";
 
   src = fetchFromGitHub {
     owner = "cinnyapp";
     repo = "cinny-desktop";
-    tag = "v${version}";
-    hash = "sha256-ls0ZxXiIrjyLL0MoxOTU/RK0k323nUiQfxtlwsEL45U=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-ORFl14JU/10u5AE7nNxg/CLpy+VxEq9lmusQrYGvD9o=";
   };
 
-  sourceRoot = "${src.name}/src-tauri";
+  sourceRoot = "${finalAttrs.src.name}/src-tauri";
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-NSzGB6o6BBoak2gbSOu8ucWA+R+behuTxeMnKpyA7no=";
+  cargoHash = "sha256-QE2I4mGrBFbXb0oQagYct8EmUO5BCJoOqi3YB2OxHd0=";
 
   postPatch =
     let
       cinny' =
         assert lib.assertMsg (
-          cinny.version == version
-        ) "cinny.version (${cinny.version}) != cinny-desktop.version (${version})";
+          cinny.version == finalAttrs.version
+        ) "cinny.version (${cinny.version}) != cinny-desktop.version (${finalAttrs.version})";
         cinny.override {
           conf = {
             hashRouter.enabled = true;
@@ -95,4 +95,4 @@ rustPlatform.buildRustPackage rec {
     platforms = lib.platforms.linux ++ lib.platforms.darwin;
     mainProgram = "cinny";
   };
-}
+})
